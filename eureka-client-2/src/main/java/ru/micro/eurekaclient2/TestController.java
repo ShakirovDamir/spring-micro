3781ba266eeda5cede99c68fba7ee9e6e818f8de
Damir <shakirov-damir@mail.ru>
2023-08-30 22:44:30 +0300

    micro

--- eureka-client-2/src/main/java/ru/micro/eurekaclient2/TestController.java
+++ eureka-client-2/src/main/java/ru/micro/eurekaclient2/TestController.java
@@ -1,19 +1,15 @@
-package ru.micro.eurekaclient;
+package ru.micro.eurekaclient2;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/test")
 public class TestController {
 
-    @Value("${eureka.instance.instance-id}")
-    private String id;
-
-    @GetMapping("/test")
+    @GetMapping("/name")
     public String test() {
-        return id;
+        return "test2!";
     }
 }
