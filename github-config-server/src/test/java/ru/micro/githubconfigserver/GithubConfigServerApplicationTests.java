71475a49abb6f5844283797e77c693eafbc94b82
Damir <shakirov-damir@mail.ru>
2023-08-31 22:36:25 +0300

    github config

---
+++ github-config-server/src/test/java/ru/micro/githubconfigserver/GithubConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.micro.githubconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
